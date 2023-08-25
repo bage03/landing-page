7f3b0486c4456f6d98daf35411e12869e9ff32e5
Ibrahim Bage <124126026+bage03@users.noreply.github.com>
2023-08-25 16:45:04 +0100

    Initial commit

---
+++ page_script.js
@@ -0,0 +1,12 @@
+  var popupLink = document.getElementById("pop_up_link");
+  var popupWindow = document.getElementById("pop_up");
+  var closeButton = document.getElementById("close-button");
+  // Show the pop-up window when the link is clicked
+  popupLink.addEventListener("click", function(event) {
+    event.preventDefault();
+    popupWindow.style.display = "block";
+  });
+  // Hide the pop-up window when the close button is clicked
+  closeButton.addEventListener("click", function() {
+    popupWindow.style.display = "none";
+  });
